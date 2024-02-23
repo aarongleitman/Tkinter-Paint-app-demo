b97a45a502236e728b326faa513c686077712773
aarongleitman <aarongleitman4@gmail.com>
2024-02-23 14:38:30 -0600

    started doing stuff

--- Paint.py
+++ Paint.py
@@ -1,86 +1,44 @@
 from tkinter import *
-from tkinter import ttk, colorchooser
+from PIL import Image, ImageTk
+import numpy as np
+import matplotlib.pyplot as plt
 
-class main:
-    def __init__(self,master):
-        self.master = master
-        self.color_fg = 'black'
-        self.color_bg = 'white'
-        self.old_x = None
-        self.old_y = None
-        self.penwidth = 5
-        self.drawWidgets()
-        self.c.bind('<B1-Motion>',self.paint)#drwaing the line 
-        self.c.bind('<ButtonRelease-1>',self.reset)
+app = Tk()
+app.geometry("400x400")
 
-    def paint(self,e):
-        if self.old_x and self.old_y:
-            self.c.create_line(self.old_x,self.old_y,e.x,e.y,width=self.penwidth,fill=self.color_fg,capstyle=ROUND,smooth=True)
+array1 = np.zeros((400,400))
 
-        self.old_x = e.x
-        self.old_y = e.y
+def append_array1(event):
+    array1[event.x,event.y] = 1
 
-    def reset(self,e):    #reseting or cleaning the canvas 
-        self.old_x = None
-        self.old_y = None      
+def get_x_and_y(event):
+    global lasx, lasy
+    lasx, lasy = event.x, event.y
 
-    def changeW(self,e): #change Width of pen through slider
-        self.penwidth = e
-           
-
-    def clear(self):
-        self.c.delete(ALL)
-
-    def change_fg(self):  #changing the pen color
-        self.color_fg=colorchooser.askcolor(color=self.color_fg)[1]
-
-    def change_bg(self):  #changing the background color canvas
-        self.color_bg=colorchooser.askcolor(color=self.color_bg)[1]
-        self.c['bg'] = self.color_bg
-
-    def drawWidgets(self):
-        self.controls = Frame(self.master,padx = 5,pady = 5)
-        Label(self.controls, text='Pen Width:',font=('arial 18')).grid(row=0,column=0)
-        self.slider = ttk.Scale(self.controls,from_= 5, to = 100,command=self.changeW,orient=HORIZONTAL)
-        self.slider.set(self.penwidth)
-        self.slider.grid(row=0,column=1,ipadx=30)
-        self.controls.pack(side=LEFT)
-        
-        self.c = Canvas(self.master,width=500,height=500,bg=self.color_bg,)
-        self.c.pack(fill=BOTH,expand=True)
-
-        menu = Menu(self.master)
-        self.master.config(menu=menu)
-        filemenu = Menu(menu)
-        colormenu = Menu(menu)
-        menu.add_cascade(label='Colors',menu=colormenu)
-        colormenu.add_command(label='Brush Color',command=self.change_fg)
-        colormenu.add_command(label='Background Color',command=self.change_bg)
-        optionmenu = Menu(menu)
-        menu.add_cascade(label='Options',menu=optionmenu)
-        optionmenu.add_command(label='Clear Canvas',command=self.clear)
-        optionmenu.add_command(label='Exit',command=self.master.destroy) 
-        
-        
-
-if __name__ == '__main__':
-    root = Tk()
-    main(root)
-    root.title('Paint App')
-    root.mainloop()
-
+def draw_smth(event):
+    global lasx, lasy
+    canvas.create_line((lasx, lasy, event.x, event.y), fill='black', width=2)
+    lasx, lasy = event.x, event.y
     
 
+canvas = Canvas(app, bg='black')
+canvas.pack(anchor='nw', fill='both', expand=1)
+
+canvas.bind("<Button-1>", get_x_and_y)
+canvas.bind("<B1-Motion>", draw_smth, append_array1)
 
 
+image = Image.open("image.jpg")
+image = image.resize((400,400))
+image = ImageTk.PhotoImage(image)
+canvas.create_image(0,0, image=image, anchor='nw')
 
 
+app.mainloop()
 
-
-
-
-
-
-
-
+def plot_binary_array(array):
+    plt.imshow(array, cmap='binary', interpolation='nearest')
+    plt.axis('off')
+    plt.show()
     
+plot_binary_array(array1)
